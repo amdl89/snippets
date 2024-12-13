b793169a1b36ac4af457d779b31a7aec382dd6e8
Amul Dahal <amul.dahal@maitriservices.com>
2024-12-13 14:10:31 +0545

    revalidate home page every 30 seconds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,21 @@ import TopicList from "@/components/topics/topic-list";
 import { fetchTopPosts } from "@/queries/posts";
 import PostList from "@/components/posts/post-list";
 
+export const revalidate = 30;
+
 export default async function Home() {
-  return (
-    <div className="grid grid-cols-4 gap-4 p-4">
-      <div className="col-span-3">
-        <h1 className="text-xl m-2">Top Posts</h1>
-        <PostList fetchData={fetchTopPosts} />
-      </div>
-      <div className="border shadow py-3 px-2">
-        <TopicCreateForm />
-        <Divider className="my-2" />
-        <h3 className="text-lg">Topics</h3>
-        <TopicList />
-      </div>
-    </div>
-  );
+	return (
+		<div className="grid grid-cols-4 gap-4 p-4">
+			<div className="col-span-3">
+				<h1 className="text-xl m-2">Top Posts</h1>
+				<PostList fetchData={fetchTopPosts} />
+			</div>
+			<div className="border shadow py-3 px-2">
+				<TopicCreateForm />
+				<Divider className="my-2" />
+				<h3 className="text-lg">Topics</h3>
+				<TopicList />
+			</div>
+		</div>
+	);
 }

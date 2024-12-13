f354e0516c429fbd313470cd73ad4d63c7f92a49
Amul Dahal <amul.dahal@maitriservices.com>
2024-12-13 11:42:36 +0545

    linter fixes

--- src/queries/comments.ts
+++ src/queries/comments.ts
@@ -2,25 +2,25 @@ import { cache } from "react";
 
 import db from "@/db";
 import { eq } from "drizzle-orm";
-import { comments, users } from "@/db/schema";
+import { comments, type users } from "@/db/schema";
 
 export type CommentWithUser = typeof comments.$inferSelect & {
-  user: Partial<typeof users.$inferInsert>;
+	user: Partial<typeof users.$inferInsert>;
 };
 
 // request memoization
 export const fetchCommentsByPostId = cache(
-  async (postId: number): Promise<CommentWithUser[]> => {
-    return await db.query.comments.findMany({
-      where: eq(comments.postId, postId),
-      with: {
-        user: {
-          columns: {
-            name: true,
-            image: true,
-          },
-        },
-      },
-    });
-  }
+	async (postId: number): Promise<CommentWithUser[]> => {
+		return await db.query.comments.findMany({
+			where: eq(comments.postId, postId),
+			with: {
+				user: {
+					columns: {
+						name: true,
+						image: true,
+					},
+				},
+			},
+		});
+	},
 );
